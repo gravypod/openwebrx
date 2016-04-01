87667dab5fcce68093ab9d6afcf2c5c0801faaa1
Joshua Katz <gravypod@gravypod.com>
2016-04-01 04:23:51 -0400

    Start a conversion to python3

--- sdrhu.py
+++ sdrhu.py
@@ -22,30 +22,31 @@
 
 import config_webrx as cfg, time, subprocess
 
+
 def run(continuously=True):
-	if not cfg.sdrhu_key: return 
-	firsttime="(Your receiver is soon getting listed on sdr.hu!)"
-	while True:
-		cmd = "wget --timeout=15 -qO- http://sdr.hu/update --post-data \"url=http://"+cfg.server_hostname+":"+str(cfg.web_port)+"&apikey="+cfg.sdrhu_key+"\" 2>&1"
-		#print "[openwebrx-sdrhu]", cmd
-		returned=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()
-		returned=returned[0]
-		#print returned
-		if "UPDATE:" in returned:
-			retrytime_mins = 20
-			value=returned.split("UPDATE:")[1].split("\n",1)[0]
-			if value.startswith("SUCCESS"):
-				print "[openwebrx-sdrhu] Update succeeded! "+firsttime
-				firsttime=""
-			else:
-				print "[openwebrx-sdrhu] Update failed, your receiver cannot be listed on sdr.hu! Reason:", value
-		else:
-			retrytime_mins = 2
-			print "[openwebrx-sdrhu] wget failed while updating, your receiver cannot be listed on sdr.hu!"
-		if not continuously: break
-		time.sleep(60*retrytime_mins)
+    if not cfg.sdrhu_key: return
+    firsttime = "(Your receiver is soon getting listed on sdr.hu!)"
+    while True:
+        cmd = "wget --timeout=15 -qO- http://sdr.hu/update --post-data \"url=http://" + cfg.server_hostname + ":" + str(
+            cfg.web_port) + "&apikey=" + cfg.sdrhu_key + "\" 2>&1"
+        # print "[openwebrx-sdrhu]", cmd
+        returned = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()
+        returned = returned[0]
+        # print returned
+        if "UPDATE:" in returned:
+            retrytime_mins = 20
+            value = returned.split("UPDATE:")[1].split("\n", 1)[0]
+            if value.startswith("SUCCESS"):
+                print("[openwebrx-sdrhu] Update succeeded! " + firsttime)
+                firsttime = ""
+            else:
+                print("[openwebrx-sdrhu] Update failed, your receiver cannot be listed on sdr.hu! Reason:", value)
+        else:
+            retrytime_mins = 2
+            print("[openwebrx-sdrhu] wget failed while updating, your receiver cannot be listed on sdr.hu!")
+        if not continuously: break
+        time.sleep(60 * retrytime_mins)
 
-if __name__=="__main__":
-	run(False)
 
-	
+if __name__ == "__main__":
+    run(False)
